79ce16704a34d0c5193528ff80b95c476656dc95
LAPTOP-8GD7ET5M\ym485 <ym485r@yahoo.co.jp>
2018-01-06 01:08:57 +0900

    Lesson5 課題・発展課題提出
    課題：CollisionCounterクラス追加　　発展課題：FripperControllerクラス修正

---
+++ Assets/CollisionCounter.cs
@@ -0,0 +1,49 @@
+﻿//////////////////////////////////////////////////////////////////////
+// 星・雲にボールを当てると得点が入り、画面に表示される
+//////////////////////////////////////////////////////////////////////
+
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class CollisionCounter : MonoBehaviour
+{
+    // 衝突時の得点合計
+    public static int CollisionCount;
+    // 衝突１回当たりの得点
+    private int Count;
+    // 得点を表示するテキスト
+    private GameObject CollisionCountText;
+
+    // Use this for initialization
+    void Start()
+    {
+        // シーンの中のCollisionCountTextオブジェクトを取得
+        this.CollisionCountText = GameObject.Find("CollisionCountText");
+
+        // ターゲットの種類によって得点を変える
+        if (tag == "SmallStarTag") {
+            this.Count = 5;
+        } else if (tag == "LargeStarTag") {
+            this.Count = 10;
+        } else if (tag == "SmallCloudTag") {
+            this.Count = 3;
+        } else if (tag == "LargeCloudTag") {
+            this.Count = 6;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    // 衝突時に呼ばれる関数
+    void OnCollisionEnter(Collision other)
+    {
+        CollisionCount = CollisionCount + this.Count;
+        this.CollisionCountText.GetComponent<Text>().text = CollisionCount + "点";
+    }
+
+}
---
+++ Assets/FripperController.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FripperController : MonoBehaviour {
+    // HingeJointコンポーネントを入れる
+    private HingeJoint myHingeJoint;
+
+    // 初期の傾き
+    private float defaultAngle = 20;
+    // 弾いた時の傾き
+    private float flickAngle = -20;
+
+    // Use this for initialization
+	void Start () {
+        // HingeJointコンポーネント取得
+        this.myHingeJoint = GetComponent<HingeJoint>();
+
+        // フリッパーの傾きを設定
+        setAngle(this.defaultAngle);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        ////////////////////////////////////////////////////////////////////////////
+        // 発展課題：画面タッチで左右のフリッパーを動かす
+        ////////////////////////////////////////////////////////////////////////////
+
+        if (Input.touchCount > 0) {
+            Touch T = Input.GetTouch(0);
+            if (T.position.x < Screen.width / 2 && tag == "LeftFripperTag"){
+                // 画面左半分にタッチしたら、左フリッパー
+                print("Left");
+                setAngle(this.flickAngle);
+            }
+
+            if (T.position.x >= Screen.width / 2 && tag == "RightFripperTag")
+            {
+                // 画面右半分にタッチしたら、右フリッパー
+                print("Right");
+                setAngle(this.flickAngle);
+            }
+
+            // 指を離した時
+            if (T.phase == TouchPhase.Ended)
+            {
+                setAngle(this.defaultAngle);
+            }
+
+        }
+
+        // 左矢印キーを押した時、左フリッパーを動かす
+        if (Input.GetKeyDown(KeyCode.LeftArrow) && tag == "LeftFripperTag")
+        {
+            setAngle(this.flickAngle);
+        }
+        // 右矢印キーを押した時、右フリッパーを動かす
+        if (Input.GetKeyDown(KeyCode.RightArrow) && tag == "RightFripperTag")
+        {
+            setAngle(this.flickAngle);
+        }
+        // 矢印キーが離された時、フリッパーを元に戻す
+        if (Input.GetKeyUp(KeyCode.LeftArrow) && tag == "LeftFripperTag")
+        {
+            setAngle(this.defaultAngle);
+        }
+        if (Input.GetKeyUp(KeyCode.RightArrow) && tag == "RightFripperTag")
+        {
+            setAngle(this.defaultAngle);
+        }
+
+    }
+
+    // フリッパーの傾きを設定
+    public void setAngle (float angle){
+        JointSpring jointSpr = this.myHingeJoint.spring;
+        jointSpr.targetPosition = angle;
+        this.myHingeJoint.spring = jointSpr;
+
+    }
+}
